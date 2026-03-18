57e25740be1e05cb9c039d7d1b6ba5cc7b1c43c2
Maksym Larchenko <maksym.larchenko.25@kse.org.ua>
2026-03-18 14:01:38 +0200

    Додано приклад створення словника

---
+++ lecture_1803.py
@@ -0,0 +1,11 @@
+rectangle = {"width" : 7, "height" : 6,
+             "color" : "blue", "coords" : [[0, 6], [7, 0]]}
+print(rectangle["height"])
+print(rectangle["coords"][1])
+print(rectangle.get("width"))
+print(rectangle.get("area"))
+if rectangle.get("area") == None:
+    rectangle["area"] = rectangle["width"]*rectangle["height"]
+print(rectangle["area"])
+#квадратні дужки coords[][] для списку в списку
+print(rectangle)
